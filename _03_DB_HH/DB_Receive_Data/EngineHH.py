a5496db4e5459bfd1650d7cf1945c6663fba667e
Evgeniy Saprykin <saprykin.ei@yandex.ru>
2025-03-21 12:15:43 +0300

    !

--- _03_DB_HH/DB_Receive_Data/EngineHH.py
+++ _03_DB_HH/DB_Receive_Data/EngineHH.py
@@ -11,6 +11,7 @@ class HH:
     def __init__(self, employer):
         self.employer = employer
         self.employer_dict = {}
+        self.vacancies_emp = []
 
     def get_employer(self):
         url = r'https://api.hh.ru/employers'
@@ -46,18 +47,29 @@ class HH:
         return data
 
 
-
-
     def get_vacancies(self, employer_id):
         vacancies_emp_dicts = []
         for page in range(10):
             vacancies_data = json.loads(self.__get_page_vacancies(employer_id, page))
             if 'errors' in vacancies_data:
                 print(vacancies_data['errors'][0]['value'])
-            # for vacancy_data in vacancies_data['items']
+            for vacancy_data in vacancies_data['items']:
+                if vacancy_data['salary'] is None:
+                    vacancy_data['salary'] = {}
+                    vacancy_data['salary']['from'] = None
+                    vacancy_data['salary']['to'] = None
+                vacancy_dict = {
+                    'id': vacancy_data['id'],
+                    'city': vacancy_data['area']['name'],
+                    'vacancy': vacancy_data['name'],
+                    'url': vacancy_data['alternate_url'],
+                    'salary_from': vacancy_data['salary']['from'],
+                    'salary_to': vacancy_data['salary']['to'],
+                    'employer_id': vacancy_data['employer']['id'],
+                }
+                self.vacancies_emp.append(vacancy_dict)
+        return  self.vacancies_emp
 
-
-            
 
 
 
@@ -68,3 +80,4 @@ if __name__ == '__main__':
     hh = HH('skyeng')
     print(hh.get_employer())
     print(HH.employers_data)
+    print(len(hh.get_vacancies('1122462')))
